b8505c3a6098904ad477253e0e72a5fa85733007
TrayanStefanov <trayanstefanov95@gmail.com>
2025-07-17 17:21:28 +0300

    Admin model initial commit.

---
+++ backend/models/admin.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const adminSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+adminSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+adminSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+const Admin = mongoose.model("Admin", adminSchema);
+export default Admin;
