5259587a3bbe300488e97d1b38fc734751d662b7
TrayanStefanov <trayanstefanov95@gmail.com>
2025-07-17 17:58:04 +0300

    Import typo/

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import Admin from "../models/admin.model";
+import Admin from "../models/admin.model.js";
 
 export const protectRoute = async (req, res, next) => {
     try{
