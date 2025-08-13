e2e45046a6acfaec9f9ad241d04ef3f1fd87c1db
TrayanStefanov <trayanstefanov95@gmail.com>
2025-08-13 16:09:50 +0300

    Footer ver 0.3.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,11 +11,22 @@ const Footer = () => {
 
     return (
         <footer className="footer bg-secondary text-primary p-6 justify-around border-t-2 border-primary">
-            <aside className="self-center navbar-start aspect-3/1 max-w-[400px] min-w-[200px]">
-                <Link to='/'>
-                    <img src={t("navbar.logo")} className="w-64 ml-4" alt="" />
-                </Link>
-            </aside>
+            <div className="flex flex-row justify-between">
+
+                <aside className="self-center navbar-start aspect-3/1 max-w-[400px] min-w-[200px]">
+                    <Link to='/'>
+                        <img src={t("navbar.logo")} className="w-40 lg:w-64 ml-4" alt="" />
+                    </Link>
+                </aside>
+                <aside className="self-center">
+                    <h6 className="footer-title">{t("footer.social")}</h6>
+                    <div className="grid grid-flow-col gap-4">
+                        <a className="md:text-3xl lg:text-5xl text-bold"><TbBrandFacebook /></a>
+                        <a href={t("footer.social-links.linkedin")} className="md:text-3xl lg:text-5xl text-bold"><TbBrandLinkedin /></a>
+                        <a className="md:text-3xl lg:text-5xl text-bold"><TbBrandTwitter /></a>
+                    </div>
+                </aside>
+            </div>
             <aside className="self-center">
                 <p className="flex flex-row text-center items-center md:text-l lg:text-2xl indent-2"><FiPhoneCall />{t("footer.phone")}</p>
                 <p className="flex flex-row text-center items-center md:text-l lg:text-2xl indent-2"><FiMail />
@@ -23,16 +34,9 @@ const Footer = () => {
                         {email}
                     </a>
                 </p>
-                <p className="md:text-l lg:text-2xl">{t("footer.slogan")}</p>
+            <p className="hidden lg:text-2xl lg:block">{t("footer.slogan")}</p>
             </aside>
-            <aside className="self-center">
-                <h6 className="footer-title">{t("footer.social")}</h6>
-                <div className="grid grid-flow-col gap-4">
-                    <a className="md:text-3xl lg:text-5xl text-bold"><TbBrandFacebook /></a>
-                    <a href={t("footer.social-links.linkedin")} className="md:text-3xl lg:text-5xl text-bold"><TbBrandLinkedin /></a>
-                    <a className="md:text-3xl lg:text-5xl text-bold"><TbBrandTwitter /></a>
-                </div>
-            </aside>
+            <p className="md:text-l lg:hidden">{t("footer.slogan")}</p>
         </footer>
     )
 
