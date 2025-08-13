40a129ff545fe685397e11b2687fd7b7a4b1df18
TrayanStefanov <trayanstefanov95@gmail.com>
2025-08-13 14:39:47 +0300

    Services page ver 0.3.

--- frontend/src/pages/ServicesPage.jsx
+++ frontend/src/pages/ServicesPage.jsx
@@ -5,6 +5,7 @@ import ServicePair from "../components/AnimationServicePair";
 
 const Services = () => {
   const { t } = useTranslation();
+  const provided = t("services.provided", { returnObjects: true });
 
   return (
     <div className='min-h-screen overflow-hidden'>
@@ -20,8 +21,8 @@ const Services = () => {
           </div>
         </div>
       </div>
-      <div className='bg-radial-accent'>
-        <h2 className='text-4xl md:text-5xl lg:text-6xl text-primary p-4 text-center mb-10'>{t("services.provided.title")}</h2>
+      <div className='lg:bg-radial-accent'>
+        <h2 className='text-4xl md:text-5xl lg:text-6xl text-primary p-4 text-center lg:mb-10'>{t("services.provided.title")}</h2>
         <div className='max-w-[80vw] mx-auto hidden md:block'>
           <div className="container mx-auto px-4">
             {[1, 2, 3, 4, 5, 6, 7].map((num, idx) => (
@@ -33,6 +34,22 @@ const Services = () => {
               />
             ))}
           </div>
+        </div>
+        <div className='mx-auto block md:hidden join join-vertical w-full rounded-none'>
+          {Object.entries(provided.leftlist).map(([key, title], index) => (
+            <div key={key} className="collapse collapse-arrow join-item border-base-300 border border-l-0 border-r-0 bg-primary text-accent">
+              <input type="radio" name="my-accordion-4" defaultChecked={index === 0} />
+              <div className="collapse-title text-xl font-medium">{title}</div>
+              <div className="collapse-content">
+                <ul className="list-disc ml-4">
+                  {provided.rightlist[key].map((item, i) => (
+                    <li key={i}>{item}</li>
+                  ))}
+                </ul>
+                <img className='mt-5' src="/services-hero.jpg" alt="" />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
