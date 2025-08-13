6a731f6f746fa5264a8eb547cb220c8674d5c910
TrayanStefanov <trayanstefanov95@gmail.com>
2025-08-13 14:39:11 +0300

    Small tweek

--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -8,7 +8,7 @@ const ProjectsPage = () => {
   let projecttitle = t("project.title");
 
   return (
-    <div className="max-w-[100vw] md:max-w-[90vw] lg:max-w-[75vw] mx-auto lg:p-6 rounded-lg bg-[linear-gradient(theme(colors.secondary),theme(colors.white))]">
+    <div className="max-w-[100vw] md:max-w-[90vw] lg:max-w-[75vw] mx-auto lg:p-6 pt-24 pb-10">
       <img src={projectimg} alt={projecttitle} className="w-[98vw] lg:w-[70vw] h-[40vh] lg:h-[60vh] object-cover rounded-lg mb-10 justify-self-center" />
       <h1 className="text-5xl font-semibold my-10 mx-6 text-center">{t("project.title")}</h1>
       <h2 className="text-2xl mb-4 indent-4 italic mx-6">{t("project.tags")} - {t("project.time")}</h2>
