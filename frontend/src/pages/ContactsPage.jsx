852ef1e888383ffa1765d41230c804c7615c680d
TrayanStefanov <trayanstefanov95@gmail.com>
2025-08-23 21:17:47 +0300

    Contacts page v0.6

--- frontend/src/pages/ContactsPage.jsx
+++ frontend/src/pages/ContactsPage.jsx
@@ -10,26 +10,26 @@ const Contacts = () => {
   const email = `${emailUser}@${emailDomain}`;
 
   return (
-    <div className="overflow-hidden pt-40 pb-10">
+    <div className="overflow-hidden pt-[15vh] min-h-[80vh] content-center font-bn">
       <div className="flex flex-col bg-[radial-gradient(circle,theme(colors.secondary),theme(colors.white))]">
         <div className="flex flex-col-reverse lg:flex-row">
           {/* LEFT SIDE: Contact details */}
           <div className="min-h-[40vh] lg:w-1/2 text-center flex flex-col justify-center items-center text-primary p-6">
             <h1 className="text-4xl lg:text-5xl mb-6">{t("contacts.title")}</h1>
-            <p className="mb-6 max-w-[80vw]">
+            <p className="text-lg max-w-[80vw]">
               {t("contacts.text.normal")}
-              <b className="ml-2">{t("contacts.text.bold")}</b>
             </p>
+              <b className="mb-6 ml-2 text-2xl text-neutral">{t("contacts.text.bold1")}, {t("contacts.text.bold2")} {t("contacts.text.bold3")}</b>
 
             <div className="flex flex-col gap-6 w-full max-w-lg">
               <h2 className="text-lg lg:text-2xl flex items-center justify-center">
-                <FiPhoneCall className="mx-2" />
+                <FiPhoneCall className="mx-2 text-neutral" />
                 <b className="mx-2">{t("contacts.phone.note")}</b>
                 {t("contacts.phone.value")}
               </h2>
 
               <h2 className="text-lg lg:text-2xl flex items-center justify-center">
-                <FiMail className="mx-2" />
+                <FiMail className="mx-2 text-neutral" />
                 <b className="mx-2">{t("contacts.email.note")}</b>
                 <a
                   href={`mailto:${email}`}
@@ -42,7 +42,7 @@ const Contacts = () => {
           </div>
 
           {/* RIGHT SIDE: Google Map */}
-          <div className="lg:w-1/2 w-full h-[300px] lg:h-auto">
+          <div className="lg:w-1/2 w-full h-[300px] lg:h-auto mx-10">
             <iframe
               title="Business area"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d93836.37935954207!2d23.241546740875236!3d42.69552878928868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa8682cb317bf5%3A0x400a01269bf5e60!2sSofia!5e0!3m2!1sen!2sbg!4v1755696992586!5m2!1sen!2sbg"
